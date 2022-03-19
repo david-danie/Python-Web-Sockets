47c523079b1d526f4ef60456b2746f7e8c84ee18
david-danie <david.danielhdz@gmail.com>
2022-03-19 03:32:15 -0600

    3er hilo para manejar respuestas

--- servidor2.py
+++ servidor2.py
@@ -2,6 +2,7 @@ from socket import socket
 from threading import Thread, Lock
 import time
 import sys
+MAX_JUEGOS = 5
 # Se importa socket, Thread y time
 
 socketServidor = socket()
@@ -10,24 +11,26 @@ port = 9999
 socketServidor.bind((host, port))
 socketServidor.listen(5)
 noConexiones = 0
-resClientes = ['0', '0']
+# Respuesta actual - JJ/Ronda - Record -
+resClientes = ['0', '0', '0']
+#resCliente2 = ['0', '0', '0']
 
 noConexiones = 0
 def hilo_imp():
     while True:
         time.sleep(5)
-        #print(f'C1: {resClientes[0]} | C2: {resClientes[1]}.')
         if resClientes[0] == resClientes[1]:
-            print('Empate!')
+            resClientes[2] = '-'
         else:
             if resClientes[0] == '1' and resClientes[1] == '2':
-                print('Gano 2')
+                resClientes[2] = 'x'
             elif resClientes[0] == '2' and resClientes[1] == '3':
-                print('Gano 2')
+                resClientes[2] = 'x'
             elif resClientes[0] == '3' and resClientes[1] == '1':
-                print('Gano 2')
+                resClientes[2] = 'x'
             else:
-                print('Gano 1')
+                resClientes[2] = '*'
+        
         print(f'C1: {resClientes[0]} | C2: {resClientes[1]}.')
 
 def hilo_cliente(sock, noCliente):
@@ -41,7 +44,9 @@ def hilo_cliente(sock, noCliente):
     disponible = '0'
 
     while not salir:
+        resultado = resClientes[2]
         res = str(sock.recv(25))
+        #print(res)
         resJuego = res[21]
         
         if noConexiones == 2:
@@ -51,7 +56,11 @@ def hilo_cliente(sock, noCliente):
         else:
             msg = f'[{noCliente}] En espera'
             
-        msg = f'[{noCliente}] DE:{disponible}|JJ:{noJuegos}|RE:{resJuego}'
+        if noCliente == '2' and  resClientes[2] == 'x':
+            resultado = '*' 
+        elif noCliente == '2' and  resClientes[2] == '*':
+            resultado = 'x'
+        msg = f'[{noCliente}] DE:{disponible}|JJ:{noJuegos}|RE:{resJuego}|RS:{resultado}'
         sock.send(msg.encode())
 
         if resJuego == '1':
@@ -75,7 +84,7 @@ if __name__ == '__main__':
     t3 = Thread(target = hilo_imp)
     t3.start()
     while True:
-        print(f'Esperando conexion. C1: {resClientes[0]} | C2: {resClientes[1]}.')
+        print(f'Esperando conexion. C1: {resClientes[0]} | C2: {resClientes[1]}.')     
         try:
             conn, addr = socketServidor.accept()
         except:
